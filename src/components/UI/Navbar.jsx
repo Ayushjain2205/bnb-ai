553dc5d6ca19184b11dbcd4c44d49fb49ce1dbed
Ayush Jain <ayushjain2205@gmail.com>
2024-03-02 23:40:14 +0530

    Configure thirdweb and connect wallet button

--- src/components/UI/Navbar.jsx
+++ src/components/UI/Navbar.jsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ConnectWallet } from "@thirdweb-dev/react";
+import { ModeContext } from "../../contexts/ModeContext";
 
 const Navbar = () => {
+  const { mode } = useContext(ModeContext);
   return (
     <nav className="flex justify-between items-center h-[56px]">
       <div className="flex-1">
         <span className="text-[32px] font-bold text-primary">opBNB.ai</span>
       </div>
       <div className="flex flex-row gap-[24px]">
-        <button className="btn btn-primary rounded-none bg-primary h-[48px] w-[192px] font-bold text-white">
-          Connect wallet
-        </button>
+        <ConnectWallet
+          style={{
+            backgroundColor: mode === "light" ? "#F3BA2F" : "#35363B",
+            borderRadius: "0px",
+            height: "48px",
+            width: "192px",
+            border: "none",
+            color: "white",
+            fontWeight: "bold",
+          }}
+        />
         <div className="flex flex-row items-center justify-center rounded-[100px] bg-primary h-[48px] w-[48px]">
           <img src="/icons/dude.svg" alt="" />
         </div>
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
 import "@/styles/globals.css";
 import { ModeContext } from "../contexts/ModeContext";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
 
 export default function App({ Component, pageProps }) {
   const [mode, setMode] = useState("light");
 
   return (
-    <ModeContext.Provider value={{ mode, setMode }}>
-      <Component {...pageProps} />
-    </ModeContext.Provider>
+    <ThirdwebProvider
+      activeChain="ethereum"
+      clientId="9d7f6362bf8c29479b09b0c2a0573a24"
+    >
+      <ModeContext.Provider value={{ mode, setMode }}>
+        <Component {...pageProps} />
+      </ModeContext.Provider>
+    </ThirdwebProvider>
   );
 }
